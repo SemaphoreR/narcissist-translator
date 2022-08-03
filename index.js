38ac1b1e02623ec2dbe480f202b6337c0216f468
Semaphore <kristen.m.eversole@gmail.com>
2022-08-03 18:37:15 -0500

    Reorder code for readability

--- index.js
+++ index.js
@@ -1,55 +1,3 @@
-
-function rand(n) {
-  return Math.floor(Math.random()*n)
-}
-
-let ungratefulChild = function() {
-  let response =[
-  "you WORTHLESS PEON ",
-  "you UNGRATEFUL CHILD ",
-  "you MONSTER "]
-  finalString += response[rand(3)]
-}
-let faultyMemory = function() {
-  let response =[
-  "gaslight gaslight gaslight ",
-  "your memories are incompatible with my self-image and therefore wrong ",
-  "how DARE you try to hold me responsible for my actions ",
-  "I haven't done a single thing wrong in my entire life "]
-  finalString += response[rand(4)]
-}
-let butFamily= function() {
-  let response =[
-  "but FAAAAAAAAMILY ",
-  "fAmIlYYYyyyyyYYYYy ",
-  "but but but FAAAAAAMILYYYYYYY "]
-  finalString += response[rand(3)]
-}
-let fakeApology = function() {
-  let response =[
-  "the word 'sorry' magically makes everything okay right ",
-  "let me offer an apology you know I don't mean but please come back I need more puppets to manipulate ",
-  "I have learned nothing but you're supposed to forgive me anyway because I'm the very definition of entitled "]
-  finalString += response[rand(3)]
-}
-let loveBomb = function() {
-  let response =[
-  "if I lovebomb you enough will you give me access to that sweet sweet N-supply ",
-  "maybe if I say the word 'love' enough you'll forget that I'm an abusive monster ",
-  "combination lovebomb/guilt-trip "]
-  finalString += response[rand(3)]
-}
-let insertBlahs = function() {
-  let ranN = (rand(10) + 1);
-  if (ranN === 10) {
-    finalString += "harrumph ";
-  } else if (ranN >= 5) {
-    finalString += "blah ";
-  } else {
-// nothing
-  }
-}
-
 let finalString = "";
 
 let narcTranslator = function() {
@@ -123,12 +71,69 @@ let narcTranslator = function() {
     document.getElementById("output").value = finalString + meString;
 }
 
+function rand(n) {
+  return Math.floor(Math.random()*n)
+}
+
+let ungratefulChild = function() {
+  let response =[
+  "you WORTHLESS PEON ",
+  "you UNGRATEFUL CHILD ",
+  "you MONSTER "]
+  finalString += response[rand(3)]
+}
+
+let faultyMemory = function() {
+  let response =[
+  "gaslight gaslight gaslight ",
+  "your memories are incompatible with my self-image and therefore wrong ",
+  "how DARE you try to hold me responsible for my actions ",
+  "I haven't done a single thing wrong in my entire life "]
+  finalString += response[rand(4)]
+}
+
+let butFamily= function() {
+  let response =[
+  "but FAAAAAAAAMILY ",
+  "fAmIlYYYyyyyyYYYYy ",
+  "but but but FAAAAAAMILYYYYYYY "]
+  finalString += response[rand(3)]
+}
+
+let fakeApology = function() {
+  let response =[
+  "the word 'sorry' magically makes everything okay right ",
+  "let me offer an apology you know I don't mean but please come back I need more puppets to manipulate ",
+  "I have learned nothing but you're supposed to forgive me anyway because I'm the very definition of entitled "]
+  finalString += response[rand(3)]
+}
+
+let loveBomb = function() {
+  let response =[
+  "if I lovebomb you enough will you give me access to that sweet sweet N-supply ",
+  "maybe if I say the word 'love' enough you'll forget that I'm an abusive monster ",
+  "combination lovebomb/guilt-trip "]
+  finalString += response[rand(3)]
+}
+
+let insertBlahs = function() {
+  let ranN = (rand(10) + 1);
+  if (ranN === 10) {
+    finalString += "harrumph ";
+  } else if (ranN >= 5) {
+    finalString += "blah ";
+  } else {
+// nothing
+  }
+}
+
 let reset = function() {
   document.getElementById("input").value = ""
   document.getElementById("output").value = ""
   finalString = ""
 }
 
+// event listeners
 const popup = document.querySelector(".popup");
 const trigger = document.querySelector("#feedback-button");
 const closeButton = document.querySelector("#close-button");
@@ -148,7 +153,6 @@ closeButton.addEventListener("click", togglePopup);
 window.addEventListener("click", windowOnClick);
 
 
-
 const toggleSwitch = document.querySelector('.theme-switch input[type="checkbox"]');
 
 function switchTheme(e) {
@@ -156,12 +160,12 @@ function switchTheme(e) {
         document.documentElement.setAttribute('data-theme', 'toddler');
     }
     else {
-        document.documentElement.setAttribute('data-theme', 'light');
+        document.documentElement.setAttribute('data-theme', 'normal');
     }
 }
 
 toggleSwitch.addEventListener('change', switchTheme, false);
-  document.getElementById("reset-button").addEventListener ("click", reset, false);
-  document.getElementById("translate-button").addEventListener ("click", narcTranslator, false);
-  document.getElementById("mode-change").addEventListener("click", changeTheme, false);
+document.getElementById("reset-button").addEventListener ("click", reset, false);
+document.getElementById("translate-button").addEventListener ("click", narcTranslator, false);
+
 
